802ebd6e257509adb9184d169da9be5459c1a395
agustindamian258-hue <agustindamian258@gmail.com>
2026-06-25 02:10:45 -0300

    Add z coordinate and player name to PlayerState

--- server/game/PlayerState.js
+++ server/game/PlayerState.js
@@ -1,23 +1,26 @@
 class PlayerState {
-  constructor(id, x, y) {
+  constructor(id, x, y, z, name) {
     this.id = id;
-    this.x = x;
-    this.y = y;
-    this.angle = 0;
+    this.name = name || 'Player_' + id.slice(0, 4);
+    this.x = x || 0;
+    this.y = y || 1.7;
+    this.z = z || 0;
+    this.rotY = 0;
     this.health = 100;
     this.maxHealth = 100;
     this.kills = 0;
     this.deaths = 0;
+    this.alive = true;
     this.lastUpdate = 0;
     this.lastFired = 0;
     this.joinedAt = Date.now();
-    this.alive = true;
   }
 
-  updatePosition(x, y, angle) {
+  updatePosition(x, y, z, rotY) {
     this.x = x;
     this.y = y;
-    this.angle = angle;
+    this.z = z;
+    this.rotY = rotY;
     this.lastUpdate = Date.now();
   }
 
@@ -27,18 +30,19 @@ class PlayerState {
     if (this.health <= 0) {
       this.alive = false;
       this.deaths++;
-      return true; // murió
+      return true;
     }
     return false;
   }
 
   respawn() {
-    this.x = 100 + Math.floor(Math.random() * 1400);
-    this.y = 100 + Math.floor(Math.random() * 1400);
+    this.x = (Math.random() - 0.5) * 100;
+    this.y = 1.7;
+    this.z = (Math.random() - 0.5) * 100;
     this.health = this.maxHealth;
     this.alive = true;
     this.lastUpdate = Date.now();
-    console.log(`[PlayerState] Respawn de ${this.id} en (${this.x}, ${this.y})`);
+    console.log(`[PlayerState] Respawn de ${this.name} en (${this.x.toFixed(0)}, ${this.z.toFixed(0)})`);
   }
 
   addKill() {
@@ -48,9 +52,11 @@ class PlayerState {
   serialize() {
     return {
       id: this.id,
+      name: this.name,
       x: this.x,
       y: this.y,
-      angle: this.angle,
+      z: this.z,
+      rotY: this.rotY,
       health: this.health,
       kills: this.kills,
       deaths: this.deaths,
